e7e006fdc111f9b1b06f9adb2a7ef43e70c80d72
abdullahahadi <abdullah.ahadi78@gmail.com>
2026-03-20 06:25:38 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+
+ctx.fillStyle = "darkblue";
+ctx.fillRect(0, 0, width, height);
+
