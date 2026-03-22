f7d0123873bf712b52e01580ca4e4461623090b7
abdullahahadi <abdullah.ahadi78@gmail.com>
2026-03-22 19:32:41 +0430

    adding computer control

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
-
+const MAX_COMPUTER_SPEED = 2;
 
 const BALL_SIZE = 5;
-let ballPosition = {
-    x: 20,
-    y: 30
-};
 
-let xSpeed = 4;
-let ySpeed = 2;
+let ballPosition;
+
+let xSpeed;
+let ySpeed;
+
+function initBall() {
+
+ballPosition = {x: 20, y: 30};
+xSpeed = 4;
+ySpeed = 2;
+
+}
 
 const PADDLE_WIDTH = 5;
 const PADDLE_HEIGHT = 20;
@@ -51,11 +57,30 @@ function draw() {
 
 }
 
-draw();
+function followBall() {
+    let ball = {
+        top : ballPosition.y,
+        bottom : ballPosition.y + BALL_SIZE
+    };
+    let leftPaddle = {
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT
+    }
+
+    if(ball.top < leftPaddle.top) {
+        leftPaddleTop -= MAX_COMPUTER_SPEED;
+    }else if(ball.bottom > leftPaddle.bottom){
+        leftPaddleTop += MAX_COMPUTER_SPEED
+    }
+}
 
 function update() {
+    // Human player
     ballPosition.x += xSpeed;
     ballPosition.y += ySpeed;
+
+    // computer play
+    followBall(); 
 }
 
 function checkPaddleCollision(ball, paddle) {
@@ -118,8 +143,13 @@ function adjustAngle(distanceFromTop, distanceFromBottom) {
             xSpeed = -Math.abs(xSpeed);
         }
 
-        if (ball.left < 0 || ball.right > width) {
-            xSpeed = -xSpeed;
+        if (ball.left < 0) {
+            rightScore++;
+            initBall();
+        }
+        if (ball.right > width) {
+            leftScore++;
+            initBall();
         }
         if (ball.top < 0 || ball.bottom > height) {
             ySpeed = -ySpeed;
@@ -131,7 +161,8 @@ function adjustAngle(distanceFromTop, distanceFromBottom) {
         update();
         checkCollision();
 
+        // Call this function again after a timeout 
         setTimeout(gameLoop, 60)
     }
-    // Call this function again after a timeout 
+    initBall();
     gameLoop();
